997b7c8467b0b64520cc4eed8e93e75a95a70399
Chris Desautels <Desautels_Christopehr@bah.com>
2019-05-20 12:09:31 -0400

    Tweaks to asset export command

--- devutils/zc-export-app-assets-to-path.sh
+++ devutils/zc-export-app-assets-to-path.sh
@@ -9,17 +9,17 @@ echo "Started $0 v$VERSIONINFO"
 source $HOME/zcmd/devutils/default-docker-env.txt
 
 PROJECTNAME=$1
-TARGET_DOCROOT_PATH=$2
+SOURCE_DOCROOT_PATH=$2
 
-S3LISTER="~/zcmd/devutils/s3listing.sh"
-S3GETTER="~/zcmd/devutils/s3get-sudo.sh"
+# S3LISTER="~/zcmd/devutils/s3listing.sh"
+# S3GETTER="~/zcmd/devutils/s3get-sudo.sh"
 
 function showUsage
 {
-    echo "PURPOSE: Use aws sync to install drupal asset files into local target"
-    echo "USAGE: $0 PROJECT_NAME TARGET_DOCROOT_PATH [SUFFIX]"
+    echo "PURPOSE: Use aws sync to export drupal asset files to s3"
+    echo "USAGE: $0 PROJECT_NAME SOURCE_DOCROOT_PATH [SUFFIX]"
     echo "   PROJECT_NAME = Project name in stack.env"
-    echo "   TARGET_PATH = Where we will place the files"
+    echo "   SOURCE_PATH = Where we get the files"
     echo "   SUFFIX = Suffix constructing the name of the asset file folder on S3"
     echo
     echo "NOTE S3 FOLDER NAME LOGIC: PROJECT_NAME-assets-SUFFIX"
@@ -27,15 +27,15 @@ function showUsage
 }
 
 if [ $# -lt 2 ]; then
-    echo "ERROR: Missing required TARGET_DOCROOT_PATH!"
+    echo "ERROR: Missing required SOURCE_DOCROOT_PATH!"
     showUsage
     exit 1
 fi
 
-if [ ! -d "${TARGET_DOCROOT_PATH}" ]; then
+if [ ! -d "${SOURCE_DOCROOT_PATH}" ]; then
     echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-    echo "FATAL ERROR -- Invalid TARGET_DOCROOT_PATH!"
-    echo "            ${TARGET_DOCROOT_PATH}"
+    echo "FATAL ERROR -- Invalid SOURCE_DOCROOT_PATH!"
+    echo "            ${SOURCE_DOCROOT_PATH}"
     echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
     exit 2
 fi
@@ -43,7 +43,7 @@ fi
 
 function getHostPath()
 {
-    ASSETS_HOST_PATH="${TARGET_DOCROOT_PATH}/sites/default/files"
+    ASSETS_HOST_PATH="${SOURCE_DOCROOT_PATH}/sites/default/files"
 }
 
 getHostPath
@@ -58,55 +58,41 @@ S3_URI="$SHARED_S3_ASSETS_BUCKET"
 
 S3_ASSET_SYNC_FULLDIR="${S3_DIRECTORY_NAME}"
 
-S3_SOURCE="${S3_URI}/${S3_ASSET_SYNC_FULLDIR}"
-TARGET_PATH="$ASSETS_HOST_PATH"
-SOURCE_PATH="$S3_SOURCE"
+S3_TARGET="${S3_URI}/${S3_ASSET_SYNC_FULLDIR}"
+SOURCE_PATH="$ASSETS_HOST_PATH"
+TARGET_PATH="$S3_TARGET"
 
 echo " "
-echo "ASSETS INSTALLATION"
-echo "... into $TARGET_PATH"
+echo "ASSETS EXPORT"
 echo "... from $SOURCE_PATH"
+echo "... into $TARGET_PATH"
 
 echo
-# echo "CHECKING SOURCE EXISTS..."
-# GREP=" | grep 'dumps'"
-# #IMPORTANT TO PUT THE SLASH AT THE START and END FOR THE GREP CHECK!!!!!!!!!!!!!!
-# CMD_GREPEXISTS="aws s3 ls ${S3_SOURCE} --recursive | grep '$S3_ASSET_SYNC_FULLDIR/'"
-# GREPEXISTS_RESULT=$(eval "$CMD_GREPEXISTS")
-# if [ -z "$GREPEXISTS_RESULT" ]; then
-#     echo
-#     echo "ERROR: There is no content at $SOURCE_PATH"
-#     echo "       We abort now otherwise all target content would be deleted!"
-#     echo "NOTE: Tested with command $CMD_GREPEXISTS"
-#     echo
-#     exit 111
-# fi
-# echo "FOUND SOURCE CONTENT!"
 
-CMD_S3="aws s3 sync ${ASSETS_HOST_PATH} ${S3_SOURCE} --delete"
+CMD_S3="aws s3 sync ${ASSETS_HOST_PATH} ${S3_TARGET} --delete"
 echo
 echo "SYNC CMD: $CMD_S3"
 
 echo " "
 echo "#####################################################################"
-echo "This will REPLACE the asset files in $S3_SOURCE"
+echo "This will REPLACE the asset files in $S3_TARGET"
 read -p "Press any key to continue (or CTRL-C to abort now)... " -n1 -s
 echo
 
 #Sync the files to S3 bucket now
-echo "Syncing our $S3_SOURCE into ${ASSETS_HOST_PATH}"
+echo "Syncing our $ASSETS_HOST_PATH into ${S3_TARGET}"
 ($CMD_S3)
 CMD_STATUS=$?
 if [ $CMD_STATUS -ne 0 ]; then
     echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-    echo "ERROR: Failed to sync ${ASSETS_HOST_PATH} from ${S3_SOURCE}!"
+    echo "ERROR: Failed to sync ${ASSETS_HOST_PATH} from ${S3_TARGET}!"
     echo "THE FAILED SYNC CMD: $CMD_S3"
     echo "THE EXIT CODE: $CMD_STATUS"
     echo "TIP: Check permissions of target folder."
     exit $CMD_STATUS
 fi
 
-echo "Installed ${TARGET_PATH}"
+echo "Exported to ${TARGET_PATH}"
 
 echo "#####################################################################"
 echo "Completed $0"
