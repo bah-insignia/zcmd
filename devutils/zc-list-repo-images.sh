af0a5c337877732442ce274307d8aa6a46e0f1cb
Frank Font <frankfont123@gmail.com>
2021-06-06 08:35:34 -0400

    better echo message

--- devutils/zc-list-repo-images.sh
+++ devutils/zc-list-repo-images.sh
@@ -2,7 +2,7 @@
 source $HOME/zcmd/devutils/default-docker-env.txt
 
 if [ -z "$PRIVATE_DOCKER_FULL_REGISTRY_URL" ]; then
-    echo No other docker registry found
+    echo No external docker registry found
 else
     echo Listing from our private Docker Registry....
     CMD="curl -4 ${PRIVATE_DOCKER_FULL_REGISTRY_URL}/v2/_catalog --insecure"
