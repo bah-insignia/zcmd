ac709291ce406b4c325bdaadcc2ac062cdbf3f4e
Frank Font <frankfont123@gmail.com>
2021-06-06 07:32:45 -0400

    skip docker registry if var blank

--- devutils/zc-list-repo-images.sh
+++ devutils/zc-list-repo-images.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 source $HOME/zcmd/devutils/default-docker-env.txt
 
-echo Listing from our private Docker Registry....
-CMD="curl -4 ${PRIVATE_DOCKER_FULL_REGISTRY_URL}/v2/_catalog --insecure"
-echo $CMD
-eval $CMD
+if [ -z "$PRIVATE_DOCKER_FULL_REGISTRY_URL" ]; then
+    echo No other docker registry found
+else
+    echo Listing from our private Docker Registry....
+    CMD="curl -4 ${PRIVATE_DOCKER_FULL_REGISTRY_URL}/v2/_catalog --insecure"
+    echo $CMD
+    eval $CMD
+fi
 #curl -4 ${PRIVATE_DOCKER_FULL_REGISTRY_URL}/v2/_catalog
 
 
